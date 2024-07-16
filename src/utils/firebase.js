f822e0890f64d159a2983d1b27ffb08a92290ebe
g0k01ub <ganesh.pavan.kumar@walmart.com>
2024-07-16 22:43:37 +0530

    firebase netflixpavangpt added

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDhfEWnWHASNkBUNL9tYMaQgvsw5lILjVs",
-  authDomain: "netflixgpt-601e9.firebaseapp.com",
-  projectId: "netflixgpt-601e9",
-  storageBucket: "netflixgpt-601e9.appspot.com",
-  messagingSenderId: "116676602366",
-  appId: "1:116676602366:web:9a93dc3edf1370ea6ef8af",
-  measurementId: "G-0W6VNEPDKT",
+  apiKey: "AIzaSyCSjTqlkR4qRHKxli5yslz_m3o6VhOkfg8",
+  authDomain: "netflixpavangpt.firebaseapp.com",
+  projectId: "netflixpavangpt",
+  storageBucket: "netflixpavangpt.appspot.com",
+  messagingSenderId: "297167741744",
+  appId: "1:297167741744:web:5727830c53be94cb309ef5",
+  measurementId: "G-BLHZ77FR8T",
 };
 
 // Initialize Firebase
